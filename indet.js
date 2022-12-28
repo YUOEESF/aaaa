698d48396efc015bbd05344add19200b9558e4ba
YUOEESF <121518927+YUOEESF@users.noreply.github.com>
2022-12-28 10:39:01 +0200

    Add files via upload

---
+++ indet.js
@@ -0,0 +1,54 @@
+//const firsDiv=document.querySelector("div");
+//firsDiv.style.color="red";
+
+
+//const irsDiv=document.querySelectorAll("p");
+//for(i of irsDiv){
+   // i.style.color="red";
+
+//}
+
+
+// const  but=document.querySelector("button");
+// const p= document.querySelector("h1")
+// but.addEventListener('click',foo)
+//   function foo(){
+//     p.textContent="wadi and love red"
+// }
+ 
+let body =document.querySelector('body')
+
+const h1 =document.createElement("h1")
+h1.textContent="hello barzel"
+body.appendChild(h1)
+
+const div1 =document.createElement("div")
+body.appendChild(div1)
+
+
+const p1 =document.createElement("p")
+p1.textContent="hwllo word"
+div1 .appendChild(p1)
+
+
+const div2  =document.createElement("div")
+div2.textContent="hwllo word"
+body .appendChild(div2)
+
+
+
+const p2 =document.createElement("p")
+p2.textContent="p3"
+body .appendChild(p2)
+
+const div3 =document.createElement("div")
+div3.textContent="div toow"
+body .appendChild(div3)
+
+const div4 =document.createElement("div")
+div4.textContent="div yuoesf"
+body .appendChild(div4)
+
+const button =document.createElement("button")
+button.textContent=("click abu alata")
+body.appendChild(button)
